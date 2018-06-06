e15732c96f612f455948effb4eef0e8b3a01f7d8
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-06-05 23:23:04 -0400

    Making structure more modular

--- l3deepiot/model.py
+++ l3deepiot/model.py
@@ -8,7 +8,6 @@ TODO:       Eventually get rid of this; integrate DeepIoT seamlessly with ../l3e
 '''
 Imports
 '''
-from l3embedding.training_utils import multi_gpu_model
 from keras.models import Model
 from keras.layers import Input, Conv2D, BatchNormalization, MaxPooling2D, \
     Flatten, Activation, concatenate, Dense
@@ -17,113 +16,7 @@ from kapre.time_frequency import Melspectrogram
 import tensorflow as tf
 import keras.regularizers as regularizers
 from .DeepIoT_dropOut import dropout as DeepIoT_dropout
-
-'''
-DeepIoT constants: added by Dhrubo
-'''
-
-CONV_KEEP_PROB = 0.8
-
-# Number of filters
-n_filter_a_1 = 64
-n_filter_a_2 = 128
-n_filter_a_3 = 256
-n_filter_a_4 = 512
-
-# Size of filters
-filt_size_a_1 = (3, 3)
-filt_size_a_2 = (3, 3)
-filt_size_a_3 = (3, 3)
-filt_size_a_4 = (3, 3)
-
-# Creating prob_list_ variables
-prob_list_conv1 = tf.get_variable("prob_list_conv1", [1, 1, 1, n_filter_a_1], tf.float32,
-                             tf.constant_initializer(CONV_KEEP_PROB), trainable=False)
-prob_list_conv2 = tf.get_variable("prob_list_conv2", [1, 1, 1, n_filter_a_2], tf.float32,
-                                  tf.constant_initializer(CONV_KEEP_PROB), trainable=False)
-prob_list_conv3 = tf.get_variable("prob_list_conv3", [1, 1, 1, n_filter_a_3], tf.float32,
-                                  tf.constant_initializer(CONV_KEEP_PROB), trainable=False)
-prob_list_conv4 = tf.get_variable("prob_list_conv4", [1, 1, 1, n_filter_a_4], tf.float32,
-                                  tf.constant_initializer(CONV_KEEP_PROB), trainable=False)
-
-# sol_train and prune_thresh
-sol_train = tf.Variable(0, dtype=tf.float32, trainable=False)
-prun_thres = tf.get_variable("prun_thres", [], tf.float32, tf.constant_initializer(0.0), trainable=False)
-
-# Initializing dictionaries
-prob_list_dict = {u'conv1': prob_list_conv1, u'conv2': prob_list_conv2, u'conv3': prob_list_conv3,
-                  u'conv4': prob_list_conv4}
-
-org_dim_dict = {u'conv1': n_filter_a_1, u'conv2': n_filter_a_2, u'conv3': n_filter_a_3,
-                u'conv4': n_filter_a_4}
-
-layer_size_dict = {u'conv1': filt_size_a_1[0] * filt_size_a_1[1], u'conv2': filt_size_a_2[0] * filt_size_a_2[1],
-                   u'conv3': filt_size_a_3[0] * filt_size_a_3[1], u'conv4': filt_size_a_4[0] * filt_size_a_4[1]}
-
-
-'''
-Utils
-'''
-def gpu_wrapper(model_f):
-    """
-    Decorator for creating multi-gpu models
-    """
-
-    def wrapped(num_gpus=0, *args, **kwargs):
-        m, inp, out = model_f(*args, **kwargs)
-        if num_gpus > 1:
-            m = multi_gpu_model(m, gpus=num_gpus)
-
-        return m, inp, out
-
-    return wrapped
-
-###### DeepIoT Util Start
-def dropOut_prun(drop_prob, prun_thres, sol_train):
-    base_prob = 0.5
-    pruned_drop_prob = tf.cond(sol_train > 0.5,
-                               lambda: tf.where(tf.less(drop_prob, prun_thres), tf.zeros_like(drop_prob), drop_prob),
-                               lambda: tf.where(tf.less(drop_prob, prun_thres), drop_prob * base_prob, drop_prob))
-    return pruned_drop_prob
-
-
-def count_prun(prob_list_dict, prun_thres):
-    left_num_dict = {}
-    for layer_name in prob_list_dict.keys():
-        prob_list = prob_list_dict[layer_name]
-        pruned_idt = tf.where(tf.less(prob_list, prun_thres), tf.zeros_like(prob_list), tf.ones_like(prob_list))
-        left_num = tf.reduce_sum(pruned_idt)
-        left_num_dict[layer_name] = left_num
-    return left_num_dict
-
-
-def gen_cur_prun(sess, left_num_dict):
-    cur_left_num = {}
-    for layer_name in left_num_dict.keys():
-        cur_left_num[layer_name] = sess.run(left_num_dict[layer_name])
-    return cur_left_num
-
-
-def compress_ratio(cur_left_num, org_dim_dict):
-    ord_list = [u'conv1', u'conv2', u'conv3', u'conv4']
-
-    org_size = 0
-    comps_size = 0
-    for idx, layer_name in enumerate(ord_list):
-        if idx == 0:
-            org_size += layer_size_dict[layer_name] * 1 * org_dim_dict[layer_name] + org_dim_dict[layer_name]
-            comps_size += layer_size_dict[layer_name] * 1 * cur_left_num[layer_name] + cur_left_num[layer_name]
-        else:
-            last_layer_name = ord_list[idx - 1]
-            org_size += layer_size_dict[layer_name] * org_dim_dict[last_layer_name] * org_dim_dict[layer_name] + \
-                        org_dim_dict[layer_name]
-            comps_size += layer_size_dict[layer_name] * cur_left_num[last_layer_name] * cur_left_num[layer_name] + \
-                          cur_left_num[layer_name]
-    comps_ratio = float(comps_size) * 100. / org_size
-    print('Original Size:', org_size, 'Compressed Size:', comps_size, 'Left Ratio:', comps_ratio)
-    return comps_ratio
-
-###### DeepIoT Util End
+from .DeepIoT_utils import *
 
 
 '''
