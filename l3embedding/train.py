7a943d42db9fdcaa69fef5e0accb01c6ba6a6bf4
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-06-06 19:05:43 -0400

    Adding main functions for testing

--- l3embedding/train.py
+++ l3embedding/train.py
@@ -14,8 +14,8 @@ import pescador
 from skimage import img_as_float
 
 from gsheets import get_credentials, append_row, update_experiment, get_row
-from .model import MODELS, load_model
-from .audio import pcm2float
+from l3embedding.model import MODELS, load_model
+from l3embedding.audio import pcm2float
 from log import *
 import h5py
 import copy
@@ -418,3 +418,31 @@ def train(train_data_dir, validation_data_dir, output_dir,
         pickle.dump(history.history, fd)
 
     LOGGER.info('Done!')
+
+# Module test
+if __name__=='__main__':
+    print('In main')
+
+    train_data_dir = '../music_sample/train_samp'
+    validation_data_dir = '../music_sample/valid_samp'
+    output_dir = '../music_sample/out'
+    num_epochs = 1
+    train_epoch_size = 128
+    validation_epoch_size = 128
+    train_batch_size = 64
+    validation_batch_size = 64
+    model_type = 'cnn_L3_melspec2'
+    random_state = 20180123
+    learning_rate = 1e-4
+    verbose = False
+    checkpoint_interval = 10
+    log_path = None
+    disable_logging = False
+    gpus = 0
+    continue_model_dir = None
+    gsheet_id = None
+
+    train(train_data_dir=train_data_dir,validation_data_dir=validation_data_dir,output_dir=output_dir,
+          num_epochs=num_epochs, train_epoch_size=train_epoch_size, validation_epoch_size=validation_epoch_size,
+          train_batch_size=train_batch_size, validation_batch_size=validation_batch_size,
+          model_type=model_type, random_state=random_state, learning_rate=learning_rate)
