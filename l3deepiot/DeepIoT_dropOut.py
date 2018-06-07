2fdb0f8f68b18a0f925e9749d08394a70a101e77
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-06-06 20:34:29 -0400

    DeepIoT l3 with out_binary_mask handled but no compressor training yet

--- l3deepiot/DeepIoT_dropOut.py
+++ l3deepiot/DeepIoT_dropOut.py
@@ -4,8 +4,10 @@ from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import random_ops
 
+out_binary_mask={}
 
 def dropout(x, keep_prob, is_training=False, noise_shape=None, seed=None, name=None):
+    global out_binary_mask
     with ops.name_scope(name, "dropout", [x]) as name:
         x = ops.convert_to_tensor(x, name="x")
         is_training = ops.convert_to_tensor(is_training, name='is_training')
@@ -21,6 +23,6 @@ def dropout(x, keep_prob, is_training=False, noise_shape=None, seed=None, name=N
         binary_tensor = math_ops.floor(random_tensor)
         ret = tf.cond(is_training, lambda: x * binary_tensor,
                       lambda: x * keep_prob)
-
+        out_binary_mask[name[name.index('conv'):name.index('_drop')]] = binary_tensor
         ret.set_shape(x.get_shape())
-        return ret#, binary_tensor
+        return ret
