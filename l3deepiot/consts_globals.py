9e15268d1766e83ae1aa2b6551b70c3e64213e63
Dhrubojyoti Roy <royd@cse.osu.edu>
2018-06-07 15:15:07 -0400

    Renaming file; pushing TF global variables there

--- l3deepiot/consts_globals.py
+++ l3deepiot/consts_globals.py
@@ -28,16 +28,21 @@ prob_list_conv3 = tf.get_variable("prob_list_conv3", [1, 1, 1, n_filter_a_3], tf
 prob_list_conv4 = tf.get_variable("prob_list_conv4", [1, 1, 1, n_filter_a_4], tf.float32,
                                   tf.constant_initializer(CONV_KEEP_PROB), trainable=False)
 
-# sol_train and prune_thresh
+# Init global variables
+global_step = tf.Variable(0, trainable=False)
+comps_global_step = tf.Variable(0, trainable=False)
+prun_global_step = tf.Variable(0, trainable=False)
+sol_train_global_step = tf.Variable(0, trainable=False)
+
 sol_train = tf.Variable(0, dtype=tf.float32, trainable=False)
 prun_thres = tf.get_variable("prun_thres", [], tf.float32, tf.constant_initializer(0.0), trainable=False)
 
 # Initializing dictionaries
 prob_list_dict = {u'conv1': prob_list_conv1, u'conv2': prob_list_conv2, u'conv3': prob_list_conv3,
-                  u'conv4': prob_list_conv4}
+                  u'audio_embedding_layer': prob_list_conv4}
 
 org_dim_dict = {u'conv1': n_filter_a_1, u'conv2': n_filter_a_2, u'conv3': n_filter_a_3,
-                u'conv4': n_filter_a_4}
+                u'audio_embedding_layer': n_filter_a_4}
 
 layer_size_dict = {u'conv1': filt_size_a_1[0] * filt_size_a_1[1], u'conv2': filt_size_a_2[0] * filt_size_a_2[1],
-                   u'conv3': filt_size_a_3[0] * filt_size_a_3[1], u'conv4': filt_size_a_4[0] * filt_size_a_4[1]}
+                   u'conv3': filt_size_a_3[0] * filt_size_a_3[1], u'audio_embedding_layer': filt_size_a_4[0] * filt_size_a_4[1]}
